5df4de064006b5db3ee33d352346fe7ac838d992
Suranart Niamcome <suranartnc@gmail.com>
2020-09-08 17:57:42 +0700

    Add some styles

--- my_app/lib/question.dart
+++ my_app/lib/question.dart
@@ -8,6 +8,10 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Text(
+      questionText,
+      style: TextStyle(fontSize: 28),
+      textAlign: TextAlign.center,
+    );
   }
 }
