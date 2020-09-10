b9cd48272778cd032c54bdd356a4034ff39bf5ab
Suranart Niamcome <suranartnc@gmail.com>
2020-09-10 15:37:03 +0700

    Add a button to restart the quiz

--- my_app/lib/answer.dart
+++ my_app/lib/answer.dart
@@ -12,6 +12,7 @@ class Answer extends StatelessWidget {
         width: double.infinity,
         child: RaisedButton(
           color: Colors.blue,
+          textColor: Colors.white,
           child: Text(answerText),
           onPressed: selectHandler,
         ));
--- my_app/lib/result.dart
+++ my_app/lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -23,12 +24,20 @@ class Result extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text(
-          resultPhrase,
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
-      ),
+          child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text('Restart Quiz!'),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          )
+        ],
+      )),
     );
   }
 }
