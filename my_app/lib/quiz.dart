1f8dd17147b230e34e2ddc23de8b9c882657fee5
Suranart Niamcome <suranartnc@gmail.com>
2020-09-10 15:22:56 +0700

    Calculate score

--- my_app/lib/quiz.dart
+++ my_app/lib/quiz.dart
@@ -19,9 +19,10 @@ class Quiz extends StatelessWidget {
       child: Column(
         children: [
           Question(questions[questionIndex]['questionText']),
-          ...(questions[questionIndex]['answers'] as List<String>)
+          ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
               .map((answer) {
-            return Answer(answerQuestion, answer);
+            return Answer(
+                () => answerQuestion(answer['score']), answer['text']);
           }).toList()
         ],
       ),
