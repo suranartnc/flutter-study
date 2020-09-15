9db28c3b150ec1e0af387b622e80c0526ce2ddb3
Suranart Niamcome <suranartnc@gmail.com>
2020-09-15 16:35:59 +0700

    Map transactions into widgets

--- personalExpenseApp/lib/main.dart
+++ personalExpenseApp/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
+  final List<Transaction> transactions = [
     Transaction(
         id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     Transaction(id: 't2', title: 'Weekly Groceries', amount: 16.53, date: null)
@@ -35,10 +35,10 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              color: Colors.red,
-              child: Text('LIST OF TX'),
-            )
+            Column(
+                children: transactions.map((tx) {
+              return Card(child: Text(tx.title));
+            }).toList())
           ],
         ));
   }
