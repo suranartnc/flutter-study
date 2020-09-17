e20ef7a9092482adacc35c2f9f69bea315b9b329
Suranart Niamcome <suranartnc@gmail.com>
2020-09-17 19:00:54 +0700

    Add floating action button

--- personalExpenseApp/lib/main.dart
+++ personalExpenseApp/lib/main.dart
@@ -14,30 +14,34 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.add), onPressed: () {})
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // Column's width = Children' width
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                // Card's width = Parent's width
+                color: Colors.blue,
+                child: Container(child: Text('CHART!')),
+                elevation: 5,
+              ),
+            ),
+            UserTransactions()
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            // Column's width = Children' width
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  // Card's width = Parent's width
-                  color: Colors.blue,
-                  child: Container(child: Text('CHART!')),
-                  elevation: 5,
-                ),
-              ),
-              UserTransactions()
-            ],
-          ),
-        ));
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
   }
 }
