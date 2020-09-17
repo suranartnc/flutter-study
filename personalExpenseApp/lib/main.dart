8a79855417e11304867541fb53f782ee667f47c0
Suranart Niamcome <suranartnc@gmail.com>
2020-09-17 16:03:02 +0700

    Add scroll to app body to prevent soft keyboard issue

--- personalExpenseApp/lib/main.dart
+++ personalExpenseApp/lib/main.dart
@@ -15,22 +15,24 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Flutter App')),
-        body: Column(
-          // Column's width = Children' width
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                // Card's width = Parent's width
-                color: Colors.blue,
-                child: Container(child: Text('CHART!')),
-                elevation: 5,
+        body: SingleChildScrollView(
+          child: Column(
+            // Column's width = Children' width
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  // Card's width = Parent's width
+                  color: Colors.blue,
+                  child: Container(child: Text('CHART!')),
+                  elevation: 5,
+                ),
               ),
-            ),
-            UserTransactions()
-          ],
+              UserTransactions()
+            ],
+          ),
         ));
   }
 }
