2c1ec93e8ebf2132542a5998dbd29e95bf83ce11
Suranart Niamcome <suranartnc@gmail.com>
2020-09-18 18:20:22 +0700

    Set theme color

--- personalExpenseApp/lib/main.dart
+++ personalExpenseApp/lib/main.dart
@@ -8,7 +8,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter App', home: MyHomePage());
+    return MaterialApp(
+        title: 'Personal Expenses',
+        theme:
+            ThemeData(primarySwatch: Colors.purple, accentColor: Colors.amber),
+        home: MyHomePage());
   }
 }
 
@@ -56,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
